48ee5eb71e6efb344aa361d188598bb7847da8e4
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2024-06-20 01:29:13 +0900

    Generate test vectors for protected mode

--- test/rapidhash.test.ts
+++ test/rapidhash.test.ts
@@ -1,26 +1,26 @@
 import {rapidhash} from '../src/rapidhash';
-import {
-  longMessage,
-  testVectors1,
-  testVectors2,
-} from '../rapidhash-c/test_vector';
+import * as testVectorsFast from '../rapidhash-c/test_vector_fast';
 
-describe('Hash short messages', () => {
-  test.each(testVectors1)(
-    'message = "%s", seed = %s',
-    (message: string, seed: bigint, expected: bigint) => {
-      const result = rapidhash(message, {seed});
-      expect(result).toEqual(expected);
-    }
-  );
-});
+describe('rapidhash_fast', () => {
+  const {testVectors1, testVectors2, longMessage} = testVectorsFast;
 
-describe('Hash long messages', () => {
-  test.each(testVectors2)(
-    'length = %d, seed = %s',
-    (length: number, seed: bigint, expected: bigint) => {
-      const result = rapidhash(longMessage.slice(0, length), {seed});
-      expect(result).toEqual(expected);
-    }
-  );
+  describe('Hash short messages', () => {
+    test.each(testVectors1)(
+      'message = "%s", seed = %s',
+      (message: string, seed: bigint, expected: bigint) => {
+        const result = rapidhash(message, {seed});
+        expect(result).toEqual(expected);
+      }
+    );
+  });
+
+  describe('Hash long messages', () => {
+    test.each(testVectors2)(
+      'length = %d, seed = %s',
+      (length: number, seed: bigint, expected: bigint) => {
+        const result = rapidhash(longMessage.slice(0, length), {seed});
+        expect(result).toEqual(expected);
+      }
+    );
+  });
 });
