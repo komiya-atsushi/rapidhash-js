3b7137352a56c0cf844237a9df004b794858f90b
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2024-10-04 00:55:54 +0900

    Configure test (jest, attw)

--- packages/rapidhash-js/jest.config.js
+++ packages/rapidhash-js/jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
 };
