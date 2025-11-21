dde090e9efc23b17a1431e6398e9cb85e1adf105
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2025-11-21 22:45:45 +0900

    Add v3.0 benchmarks for rapidhash, including Micro and Nano variants

--- packages/benchmark/src/benchmark-bytes.ts
+++ packages/benchmark/src/benchmark-bytes.ts
@@ -1,11 +1,16 @@
+import {rapidhash_fast as rapidhash_v1_fast, rapidhash_protected as rapidhash_v1_protected} from 'rapidhash-js/v1.0';
 import {
-  rapidhash_v1_fast,
-  rapidhash_v1_protected,
-  rapidhash_v2_0_fast,
-  rapidhash_v2_0_protected,
-  rapidhash_v2_2_fast,
-  rapidhash_v2_2_protected,
-} from 'rapidhash-js';
+  rapidhash_fast as rapidhash_v2_2_fast,
+  rapidhash_protected as rapidhash_v2_2_protected,
+} from 'rapidhash-js/v2.2';
+import {
+  rapidhash_fast as rapidhash_v3_0_fast,
+  rapidhash_protected as rapidhash_v3_0_protected,
+  rapidhashMicro_fast as rapidhashMicro_v3_0_fast,
+  rapidhashMicro_protected as rapidhashMicro_v3_0_protected,
+  rapidhashNano_fast as rapidhashNano_v3_0_fast,
+  rapidhashNano_protected as rapidhashNano_v3_0_protected,
+} from 'rapidhash-js/v3.0';
 import {HashFunctionBenchmark} from './benchmark-base.js';
 import {showBenchmarkEnvironment} from './benchmark-env.js';
 import {RandomBytes} from './data.js';
@@ -51,10 +56,14 @@ async function main(): Promise<void> {
     })
     .measurementTarget('[v1.0] rapidhash (fast)', (b) => rapidhash_v1_fast(b))
     .measurementTarget('[v1.0] rapidhash (protected)', (b) => rapidhash_v1_protected(b))
-    .measurementTarget('[v2.0] rapidhash (fast)', (b) => rapidhash_v2_0_fast(b))
-    .measurementTarget('[v2.0] rapidhash (protected)', (b) => rapidhash_v2_0_protected(b))
     .measurementTarget('[v2.2] rapidhash (fast)', (b) => rapidhash_v2_2_fast(b))
     .measurementTarget('[v2.2] rapidhash (protected)', (b) => rapidhash_v2_2_protected(b))
+    .measurementTarget('[v3.0] rapidhash (fast)', (b) => rapidhash_v3_0_fast(b))
+    .measurementTarget('[v3.0] rapidhash (protected)', (b) => rapidhash_v3_0_protected(b))
+    .measurementTarget('[v3.0] rapidhashMicro (fast)', (b) => rapidhashMicro_v3_0_fast(b))
+    .measurementTarget('[v3.0] rapidhashMicro (protected)', (b) => rapidhashMicro_v3_0_protected(b))
+    .measurementTarget('[v3.0] rapidhashNano (fast)', (b) => rapidhashNano_v3_0_fast(b))
+    .measurementTarget('[v3.0] rapidhashNano (protected)', (b) => rapidhashNano_v3_0_protected(b))
     .run();
 
   showBenchmarkEnvironment();
